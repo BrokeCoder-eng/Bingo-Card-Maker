d694bffda28e11a6e79d15ba3bdcdd7360f9166b
BrokeCoder <martin.jc.pretorius@gmail.com>
2025-07-17 09:47:26 +0200

    Add pdf export and Bingo Card sheet building

--- src/BingoCard.js
+++ src/BingoCard.js
@@ -9,11 +9,64 @@ class BingoCard {
     this.height = 5;
   }
 
+  /**
+   * @returns {String} The title of the Bingo Card.
+   */
   getTitle() {
     return this.title;
   }
 
+  /**
+   * @returns {String[][]} A 2D array of the Bingo Card.
+   */
   getCells() {
     return this.cells;
+  }
+
+  /**
+   * @param {Integer} row_num
+   * @returns {String[]} An array of the specified row of the Bingo Card
+   */
+  getRow(row_num) {
+    if (row_num >= this.row.length) {
+      return this.rows[0];
+    }
+    return this.rows[row_num];
+  }
+
+  /**
+   * @param {Integer} r
+   * @param {Integer} c
+   * @returns {String} Value in the specific cell.
+   */
+  get(r, c) {
+    if (r >= this.cells.length) {
+      return this.cells[0][0];
+    }
+    else if (c >= this.cells[r].length) {
+      return this.cells[r][0];
+    }
+
+    return this.cells[r][c];
+  }
+
+  /**
+   * @param {SpreadsheetApp.Sheet} bingo_sheet
+   */
+  buildCard(bingo_sheet) {
+    try {
+      bingo_sheet.getRange("A1:E1").merge();
+
+      for (let r = 1; r <= 5; r++) {
+        bingo_sheet.setRowWidth(r, 200);
+      }
+      for (let c = 1; c <= 5; c++) {
+        bingo_sheet.setColumnWidth(c, 200);
+      }
+
+      console.log("Bingo build successful");
+    } catch(build_err) {
+      console.log("Failed to build bingo card");
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,23 +5,46 @@ const day_card = bingo.getSheetByName("Bingo Card");
 //Google Drive Folder
 const bingo_folder = DriveApp.getFolderById(BINGO_DRIVE_FOLDER_ID);
 
+function build_new_bingo_card() {
+  const blank_card = bingo.getSheetbyId(BLANK_BINGO_CARD_ID);
+  const new_card = new BingoCard(
+    blank_card.getRange("A1:E1").getValue(),
+    blank_card.getRange("A2:E6").getValues()
+  );
+
+  bingo.insertSheet(new_card.getTitle(), bingo.getSheets().length - 1);
+  const new_sheet = bingo.getSheetByName(new_card.getTitle());
+
+  new_card.buildCard(new_sheet);
+}
+
 function export_to_pdf() {
 
-const sheetId = day_card.getSheetId();
+  const sheetId = day_card.getSheetId();
 
-const exportUrl = `https://docs.google.com/spreadsheets/d/${BINGO_SPREADSHEET_ID}/export?` +
-  `format=pdf&` +
-  `size=A4&` +                       // Paper size
-  `portrait=true&` +                 // Orientation
-  `fitw=true&` +                     // Fit to width
-  `sheetnames=false&` +              // Don't show sheet names
-  `printtitle=false&` +              // Don't show title
-  `pagenumbers=false&` +             // Don't show page numbers
-  `gridlines=false&` +               // Don't show gridlines
-  `fzr=false&` +                     // Don't repeat frozen rows
-  `gid=${sheetId}`;                  // The specific sheet
+  const exportUrl = `https://docs.google.com/spreadsheets/d/${BINGO_SPREADSHEET_ID}/export?` +
+    `format=pdf&` +
+    `size=A4&` +                       // Paper size
+    `portrait=true&` +                 // Orientation
+    `fitw=true&` +                     // Fit to width
+    `sheetnames=false&` +              // Don't show sheet names
+    `printtitle=false&` +              // Don't show title
+    `pagenumbers=false&` +             // Don't show page numbers
+    `gridlines=false&` +               // Don't show gridlines
+    `fzr=false&` +                     // Don't repeat frozen rows
+    `gid=${sheetId}`;                  // The specific sheet
 
 
+  const token = ScriptApp.getOAuthToken();
+
+  const response = UrlFetchApp.fetch(exportUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    muteHttpExceptions: true
+  });
+
+  return (response.getBlob().setName("bingo_card.pdf"));
 }
 
 function email_daily_card(day_bingo_card) {
@@ -32,7 +55,9 @@ function email_daily_card(day_bingo_card) {
   }
 
   bingo_folder.setSharing(DriveApp.Access.ANYONE_WITH_LINK, DriveApp.Permission.VIEW);
-  link_to_folder = bingo_folder.getUrl();
+
+  const pdf_bingo_card = bingo_folder.createFile(export_to_pdf());
+  const pdf_link = pdf_bingo_card.getUrl();
 
   let next_person = Math.random(Math.floor(PARTICIPANTS.length));
 
@@ -45,8 +70,8 @@ function email_daily_card(day_bingo_card) {
     <p>Attached to this announcement you will find the bingo card of the relevant individual we will be focusing on. Fill it in throughout the day as you notice the patterns on the card, and be honest about what you notice.</p>
 
     <p>This is all light-hearted banter to make each day a bit more enjoyable. So please enjoy!!</p>
-    <p>Access the Bingo Card of the day here:<br>s
-    <a href="${link_to_folder}">${link_to_folder}</a></p>
+    <p>Access the Bingo Card of the day here:<br>
+    <a href="${pdf_link}">${pdf_link}</a></p>
     `;
 
     GmailApp.sendEmail(p.email, "Todays Bingo Card", "Bingo of the Day!",{ htmlBody: htmlContent });
